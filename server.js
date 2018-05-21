713e45d9116b4bddd663e0e8ac219149cf099b79
James Stuckey <jstuckey@rutgers.edu>
2018-05-21 17:16:12 -0400

    /api/notes/:id endpoint works properly

--- server.js
+++ server.js
@@ -15,6 +15,13 @@ app.get('/api/notes', (req, res) => {
     res.json(data);
 });
 
+app.get('/api/notes/:id', (req, res) => {
+    
+    const id = req.params.id;
+    const note = data.find(item => item.id === Number(id));
+    res.send(note);
+});
+
 app.listen(8080, function () {
   console.info(`Server listening on ${this.address().port}`);
 }).on('error', err => {
