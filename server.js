d6ed8ffb688a567ddb6be1a8d0e33be3919df05f
James Stuckey <jstuckey@rutgers.edu>
2018-05-22 15:00:09 -0400

    search function

--- server.js
+++ server.js
@@ -9,6 +9,8 @@ const express = require('express');
 
 const app = express();
 
+const { PORT } = require('./config');
+
 app.use(express.static('public'));
 
 app.get('/api/notes', (req, res) => {
@@ -22,18 +24,30 @@ app.get('/api/notes/:id', (req, res) => {
     res.send(note);
 });
 
-const searchTerm = req.query.searchTerm;
-if (searchTerm) {
-  let filteredList = data.filter(function(item) {
-    return item.title.includes(searchTerm);
-  });
-  res.json(filteredList);
-} else {
-  res.json(data);
-}
+app.get('/api/notes', (req, res) => {
+    const searchTerm = req.query.searchTerm;
+    if (searchTerm) {
+        let filteredList = data.filter(function(item) {
+            return item.title.includes(searchTerm);
+        });
+        res.json(filteredList);
+    } else {
+        res.json(data);
+    }
+});
 
-app.listen(8080, function () {
-  console.info(`Server listening on ${this.address().port}`);
+// const searchTerm = req.query.searchTerm;
+// if (searchTerm) {
+//   let filteredList = data.filter(function(item) {
+//     return item.title.includes(searchTerm);
+//   });
+//   res.json(filteredList);
+// } else {
+//   res.json(data);
+// }
+
+app.listen(PORT, function () {
+    console.info(`Server listening on ${this.address().port}`);
 }).on('error', err => {
-  console.error(err);
+    console.error(err);
 });
