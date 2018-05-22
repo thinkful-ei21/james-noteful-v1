9e5d22afeee9ff7a302ceb3d75aed55b95ca94e4
James Stuckey <jstuckey@rutgers.edu>
2018-05-22 15:51:34 -0400

    added an 404 handler

--- server.js
+++ server.js
@@ -6,6 +6,7 @@ const data = require('./db/notes');
 console.log('Hello Noteful!');
 
 const express = require('express');
+const logger = require('./middleware/logger');
 
 const app = express();
 
@@ -13,16 +14,7 @@ const { PORT } = require('./config');
 
 app.use(express.static('public'));
 
-app.get('/api/notes', (req, res) => {
-    res.json(data);
-});
-
-app.get('/api/notes/:id', (req, res) => {
-    
-    const id = req.params.id;
-    const note = data.find(item => item.id === Number(id));
-    res.send(note);
-});
+app.use(logger);
 
 app.get('/api/notes', (req, res) => {
     const searchTerm = req.query.searchTerm;
@@ -36,15 +28,24 @@ app.get('/api/notes', (req, res) => {
     }
 });
 
-// const searchTerm = req.query.searchTerm;
-// if (searchTerm) {
-//   let filteredList = data.filter(function(item) {
-//     return item.title.includes(searchTerm);
-//   });
-//   res.json(filteredList);
-// } else {
-//   res.json(data);
-// }
+app.get('/api/notes/:id', (req, res) => {
+    
+    const id = req.params.id;
+    const note = data.find(item => item.id === Number(id));
+    if (!note) {
+        var err = new Error('Not Found');
+        err.status = 404;
+        res.status(404).json({ message: 'Not Found' });
+    } else {
+        res.send(note);
+    }
+});
+
+app.use(function (req, res, next) {
+    var err = new Error('Not Found');
+    err.status = 404;
+    res.status(404).json({ message: 'Not Found' });
+});
 
 app.listen(PORT, function () {
     console.info(`Server listening on ${this.address().port}`);
