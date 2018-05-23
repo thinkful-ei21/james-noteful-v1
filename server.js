345422fac41bf1a152527be4cdcade6664cbcf51
James Stuckey <jstuckey@rutgers.edu>
2018-05-23 16:41:34 -0400

    moved endpoints to express router

--- server.js
+++ server.js
@@ -1,18 +1,10 @@
 'use strict';
 
-// Simple In-Memory Database
-const data = require('./db/notes');
-const simDB = require('./db/simDB');  // <<== add this
-const notes = simDB.initialize(data);
-
-console.log('Hello Noteful!');
-
 const express = require('express');
 const morgan = require('morgan');
-
 const app = express();
-
 const { PORT } = require('./config');
+const notesRouter = require('./router/notes.router');
 
 app.use(morgan('dev'));
 
@@ -20,53 +12,7 @@ app.use(express.static('public'));
 
 app.use(express.json());
 
-app.get('/api/notes', (req, res, next) => {
-    const { searchTerm } = req.query;
-  
-    notes.filter(searchTerm, (err, list) => {
-        if (err) {
-            return next(err); // goes to error handler
-        }
-        res.json(list); // responds with filtered array
-    });
-});
-
-app.get('/api/notes/:id', (req, res, next) => {
-    
-    const id = req.params.id;
-
-    notes.find(id, (err, item) => {
-        if (err) {
-            return next(err);
-        } 
-        res.json(item);
-    });
-});
-
-app.put('/api/notes/:id', (req, res, next) => {
-    const id = req.params.id;
-  
-    /***** Never trust users - validate input *****/
-    const updateObj = {};
-    const updateFields = ['title', 'content'];
-  
-    updateFields.forEach(field => {
-        if (field in req.body) {
-            updateObj[field] = req.body[field];
-        }
-    });
-    
-    notes.update(id, updateObj, (err, item) => {
-        if (err) {
-            return next(err);
-        }
-        if (item) {
-            res.json(item);
-        } else {
-            next();
-        }
-    });
-});
+app.use('/api', notesRouter);
 
 app.use(function (req, res, next) {
     var err = new Error('Not Found');
