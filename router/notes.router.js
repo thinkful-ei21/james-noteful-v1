cc540b2987379291180da33ddbc0d67805672782
James Stuckey <jstuckey@rutgers.edu>
2018-05-24 10:25:24 -0400

    finished challenge

--- router/notes.router.js
+++ router/notes.router.js
@@ -19,7 +19,7 @@ router.get('/notes', (req, res, next) => {
 
 router.get('/notes/:id', (req, res, next) => {
     
-    const id = req.params.id;
+    const {id} = req.params.id;
 
     notes.find(id, (err, item) => {
         if (err) {
@@ -50,6 +50,43 @@ router.put('/notes/:id', (req, res, next) => {
             res.json(item);
         } else {
             next();
+        }
+    });
+});
+
+// Post (insert) an item
+router.post('/notes', (req, res, next) => {
+    const { title, content } = req.body;
+  
+    const newItem = { title, content };
+    /***** Never trust users - validate input *****/
+    if (!newItem.title) {
+        const err = new Error('Missing `title` in request body');
+        err.status = 400;
+        return next(err);
+    }
+  
+    notes.create(newItem, (err, item) => {
+        if (err) {
+            return next(err);
+        }
+        if (item) {
+            res.location(`http://${req.headers.host}/notes/${item.id}`).status(201).json(item);
+        } else {
+            next();
+        }
+    });
+});
+
+router.delete('/notes/:id', (req, res, next) => {
+    const id = req.params.id;
+
+    notes.delete(id, (err, item) => {
+        if (err) {
+            return next(err);
+        } 
+        if (item) {
+            res.status(204).json(item);
         }
     });
 });
--- server.js
+++ server.js
@@ -7,17 +7,24 @@ const { PORT } = require('./config');
 const notesRouter = require('./router/notes.router');
 
 app.use(morgan('dev'));
-
 app.use(express.static('public'));
-
 app.use(express.json());
-
 app.use('/api', notesRouter);
 
-app.use(function (req, res, next) {
+
+// catches 404 errors if endpoint is not found
+app.use(function (req, res) {
     var err = new Error('Not Found');
     err.status = 404;
     res.status(404).json({ message: 'Not Found' });
+});
+
+app.use(function (err, req, res, next) {
+    res.status(err.status || 500);
+    res.json({
+        message: err.message,
+        error: err
+    });
 });
 
 app.listen(PORT, function () {
