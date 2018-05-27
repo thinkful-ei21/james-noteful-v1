8540fd3e083862b2114059bd361247abba2f5f9a
James Stuckey <jstuckey@rutgers.edu>
2018-05-26 22:49:16 -0400

    update api.js methods to return promises

--- public/scripts/api.js
+++ public/scripts/api.js
@@ -3,55 +3,92 @@
 
 const api = {
 
-    search: function (query, callback) {
-        $.ajax({
+    search: function (query) {
+        return $.ajax({
             type: 'GET',
             url: '/api/notes/',
             dataType: 'json',
             data: query,
-            success: callback
         });
     },
 
-    details: function (id, callback) {
-        $.ajax({
+    details: function (id) {
+        return $.ajax({
             type: 'GET',
             dataType: 'json',
-            url: `/api/notes/${id}`,
-            success: callback
+            url: `/api/notes/${id}`
         });
     },
 
-    update: function (id, obj, callback) {
-        $.ajax({
+    update: function (id, obj) {
+        return $.ajax({
             type: 'PUT',
             url: `/api/notes/${id}`,
             contentType: 'application/json',
             dataType: 'json',
             data: JSON.stringify(obj),
-            success: callback
         });
     },
 
-    create: function (obj, callback) {
-        $.ajax({
+    create: function (obj) {
+        return $.ajax({
             type: 'POST',
             url: '/api/notes',
             contentType: 'application/json',
             dataType: 'json',
             processData: false,
             data: JSON.stringify(obj),
-            success: callback
         });
     },
 
-    remove: function (id, callback) {
+    remove: function (id) {
         return $.ajax({
             type: 'DELETE',
             url: `/api/notes/${id}`,
             dataType: 'json',
-            success: callback
         });
     }
 
 };
+
+// // test get all
+// api.search({})
+//     .then(response => {
+//         console.log(response);
+//     });
+
+// // test get all with search term
+// api.search({searchTerm: 'cats'})
+//     .then(response => {
+//         console.log(response);
+//     });
+
+// // test details of one note
+// api.details(1005)
+//     .then(response => {
+//         console.log(response);
+//     });
+
+// // test updating one note
+// api.details(1001)
+//     .then(response => {
+//         console.log(response);
+//     });
+
+// api.update(1001, {title: 'updated title', content: 'updated content'})
+//     .then(response => {
+//         console.log(response);
+//     });
+
+// // test creating new item
+// api.create({title: 'new note title', content: 'new note content text'})
+//     .then(response => {
+//         console.log(response);
+//     });
+
+// //tests removing an item,
+// // will return undefined the first time and 404 after unless you restart the server
+// api.remove(1004)
+//     .then(response => {
+//         console.log(response);
+//     });
